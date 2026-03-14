5c019220d9eb1f10e1d338d2dfb6ebba6182d2f1
veeresh <pasareveeresh0908@gmail.com>
2026-03-14 17:17:44 +0530

    added the github portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import "./app.scss"
 import Dock from './components/Dock'
 import Nav from './components/Nav'
-import MacWindow from './components/windows/MacWindow'
+import Github from './components/github'
 const App = () => {
   return (
     <main>
       <Nav/>
       <Dock/>
 
-      <MacWindow>
-        <h1>Hello</h1>
-      </MacWindow>
+     <Github/>
     </main>
   )
 }
